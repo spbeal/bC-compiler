5ecc07409e75be2d53122643f334402fe59258d2
Samuel Beal <sampbeal@gmail.com>
2025-02-13 17:18:41 -0800

    last one

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -364,7 +364,7 @@ void codegenExpression(TreeNode * currnode)
                   emitRM((char *)"LDA", AC, currnode->offset, GP, (char *)"Load address of char array");
                }
                else {
-                  emitRM((char *)"LDC", AC, atoi((char*)currnode->attr.cvalue), AC3, (char *)"Load char constant");
+                  emitRM((char *)"LDC", AC, (int)(currnode->attr.cvalue), AC3, (char *)"Load char constant");
                }
                break;
             case Integer:
