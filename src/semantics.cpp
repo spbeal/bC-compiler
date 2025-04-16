8843455b12a836561600db5e77c86c15798e8751
Samuel Beal <sampbeal@gmail.com>
2025-04-15 17:49:48 -0700

    Idk

--- src/semantics.cpp
+++ src/semantics.cpp
@@ -718,12 +718,12 @@ void exp_traverse(TreeNode * current, SymbolTable *symtab) {
          {
             if (current->child[0] != NULL)
             {
-               // tmp = (TreeNode *)symtab->lookup(current->child[0]->attr.name);
-               // if (tmp == NULL) {
-               //    current->type = current->child[0]->type;
-               // } else {
-               //    current->type = tmp->type;
-               // }
+               tmp = (TreeNode *)symtab->lookup(current->child[0]->attr.name);
+               if (tmp == NULL) {
+                  current->type = current->child[0]->type;
+               } else {
+                  current->type = tmp->type;
+               }
             }
             else
             {
